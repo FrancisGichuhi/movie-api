ddd8c228367da311f05af2230e0aa1b8d42ec0e3
FrancisGichuhi <francisgichh@gmail.com>
2019-12-17 00:55:55 -0500

    ..

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,12 +1,12 @@
-  
+
 require('dotenv').config()
 
 module.exports = {
-    development: {
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        dialect: "mysql",
-    }
+  development: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    dialect: 'mysql',
+  },
 }
